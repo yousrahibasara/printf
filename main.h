5d4802fe7c07c708e863d03b6ae3823dab145a54
yousrahibasara <yousrahoumma@gmail.com>
2023-05-29 16:54:02 -0700

    printf project

--- main.h
+++ main.h
@@ -34,10 +34,10 @@ int (*fn)(va_list, char[], int, int, int, int);
  * @fmt: the format
  * @fm_t: the function
  */
-typedef struct fmt fmt_t
+typedef struct fmt fmt_t;
 
-int _printf(const char *format, ...);
-int handle_print(const char *fmt, int *i;
+int _printf(const char *format,...);
+int handle_print(const char fmt, int i;
 va_list list, char buffer[], int flags, int width, int precision, int size);
 
 /***************** FUNCTIONS********************/
@@ -58,8 +58,7 @@ int flags, int width, int precision, int size);
 int print_unsigned(va_list types, char buffer[],
 int flags, int width, int precision, int size);
 int print_octal(va_list types, char buffer[],
-int flags, int width, int precision, int size);
-int print_hexadecimal(va_list types, char buffer[],
+int flags, int width, int precision, int size); int print_hexadecimal(va_list types, char buffer[],
 int flags, int width, int precision, int size);
 int print_hexa_upper(va_list types, char buffer[],
 int flags, int width, int precision, int size);
@@ -81,7 +80,7 @@ int flags, int width, int precision, int size);
 int print_rot13string(va_list types, char buffer[],
 int flags, int width, int precision, int size);
 
-/* wdth handler */
+/* width handler */
 int handle_write_char(char c, char buffder[],
 int flags, int width, int precision, int size);
 int write_number(int is_positive, int ind, char buffer[],
@@ -89,9 +88,7 @@ int flags, int width, int precision, int size);
 int write_num(int ind, char buffer[], int flags, int width, int precision,
 int length, char padd, char extra_c);
 int write_pointer(char buffer[], int ind, int length,
-int width, int flags, char padd, char extra_c, int padd_start);
-
-int write_unsgnd(int is_negative, int ind,
+int width, int flags, char padd, char extra_c, int padd_start); int write_unsgnd(int is_negative, int ind,
 char buffer[],int flags, int width, int precision, int size);
 
 /******************* UTILS *****************/
@@ -102,4 +99,4 @@ int is_digit(char);
 long int convert_size_number(long int num, int size);
 long int convert_size_unsgnd(unsigned long int num, int size);
 
-#endif
+#endif /* MAIN_H */
